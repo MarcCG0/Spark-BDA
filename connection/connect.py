d2c17f71fbe336c9c13e16b6d4da9d3a3e4c46df
Marc Camps <marc.camps@bookline.ai>
2023-12-17 13:47:07 +0100

    finished without asking q

--- connection/connect.py
+++ connection/connect.py
@@ -1,23 +1,26 @@
-import os
-import sys
 from pyspark.sql import SparkSession, DataFrame
 from pyspark import SparkConf
 
 POSTGRESQL_DRIVER_PATH = "../postgresql-42.2.8.jar" 
 
 def create_session() -> SparkSession: 
-    
-    conf = SparkConf().set("spark.master", "local").set("spark.app.name", "DBALab").set("spark.jars", POSTGRESQL_DRIVER_PATH)
-    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    """Initialize the SparkSession
+    """
+    conf: SparkConf = SparkConf().set("spark.master", "local").set("spark.app.name", "DBALab").set("spark.jars", POSTGRESQL_DRIVER_PATH)
+    spark: SparkSession = SparkSession.builder.config(conf=conf).getOrCreate()
     return spark
 
 def retrieve_dw_table(db_name: str, table_instance: str, session : SparkSession,table_name:str, columns: list[str]) -> DataFrame: 
+    """Given a database name, a table instance, a spark session,
+      the name for the table you want to retrieve from the database
+        and the columns you want to retrieve from that table
+    """
 
     db_properties = {
         "driver": "org.postgresql.Driver",
-        "url": f"jdbc:postgresql://postgresfib.fib.upc.edu:6433/{db_name}?sslmode=require",  # Replace with your database URL
-        "user": "marc.camps.garreta",  # Replace with your database username
-        "password": "DB180503"  # Replace with your database password
+        "url": f"jdbc:postgresql://postgresfib.fib.upc.edu:6433/{db_name}?sslmode=require",
+        "user": "marc.camps.garreta", 
+        "password": "DB180503" 
     }
 
     data = session.read.jdbc(url=db_properties["url"],
--- core/data_management.py
+++ core/data_management.py
@@ -1,22 +1,26 @@
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark import SparkConf
-from pyspark.sql.functions import input_file_name, regexp_extract, to_date, mean, datediff, when, col, expr
-
-from  connection.connect import retrieve_dw_table, create_session
+from pyspark.sql.functions import input_file_name, regexp_extract, to_date, mean, datediff, col
+from connection.connect import retrieve_dw_table
 
 
-def get_full_csv_concatenation(session: SparkSession) -> DataFrame:
+def get_sensor_data(session: SparkSession) -> DataFrame:
+    """Returns a DataFrame that contains the mean
+      values of the sensor for a given aircraftid and date
+    """
     path_to_csv_files = "../raw_data/*.csv"  
     df = session.read.csv(path_to_csv_files, header=True, inferSchema=True, sep=";")
     pattern = r'.*\/([^\/]*)\.csv$'
     df = df.withColumn("date", to_date(df["date"]))
     df = df.withColumn("aircraftid", regexp_extract(input_file_name(), pattern, 1).substr(-6, 6))
-    df_mean = df.groupBy("date", "aircraftid").agg(mean("value").alias("mean_value"))
+    df_mean = df.groupBy("date", "aircraftid").agg(mean("value").alias("sensor_mean_value"))
     return df_mean
 
 
 def retrieve_needed_tables(spark: SparkSession) -> tuple[DataFrame, DataFrame]:
+    """Retrieves the needed tables for performing the data management
+      to obtain our matrix for training
+    """
 
     aircraft_utilization = retrieve_dw_table("DW", "public",spark,"aircraftutilization", ["timeid","aircraftid", "flighthours", "delayedminutes", "flightcycles"])
     operation_interruption = retrieve_dw_table("AMOS", "oldinstance", spark,"operationinterruption", ["subsystem","aircraftregistration", "starttime"])
@@ -25,6 +29,9 @@ def retrieve_needed_tables(spark: SparkSession) -> tuple[DataFrame, DataFrame]:
     
 
 def get_training_data(sensor_data: DataFrame, operation_interruption: DataFrame, aircraft_utilization: DataFrame)-> DataFrame:
+    """Performs the necessary transformations needed
+      for obtaining the training data matrix
+    """
 
     joined_df = sensor_data.join(
         aircraft_utilization,
@@ -35,15 +42,17 @@ def get_training_data(sensor_data: DataFrame, operation_interruption: DataFrame,
 
     operation_interruption = operation_interruption.withColumn("starttime", to_date(operation_interruption.starttime))
     operation_interruption = operation_interruption.filter(operation_interruption.subsystem == 3453)
-    operation_interruption.show()
 
     condition = (joined_df.aircraftid == operation_interruption.aircraftregistration) & \
             (datediff(operation_interruption.starttime, joined_df.date) >= 0) & \
-            (datediff(operation_interruption.starttime, joined_df.date) <= 7)
+            (datediff(operation_interruption.starttime, joined_df.date) <= 6)
 
     joined_df_with_interruption = joined_df.join(operation_interruption, condition, "left_outer")
 
-    joined_df_with_interruption = joined_df_with_interruption.withColumn("operation_interruption", (col("starttime").isNotNull()).cast("boolean"))
+    joined_df_with_interruption = joined_df_with_interruption.withColumn(
+    "operation_interruption",
+    col("starttime").isNotNull().cast("int")
+)
 
     joined_df_with_interruption = joined_df_with_interruption.drop("starttime", "subsystem", "aircraftregistration", "timeid")
 
